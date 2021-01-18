512b002b35227c3c7e6df5db0ecbc7685f6bafe9
Alan Braulio <alan-braulio@hotmail.com>
2021-01-17 21:13:05 -0300

    Iniciando o projeto React + Node JS

---
+++ client/src/routes/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from '../Components/Home/Home';
+// import { UserStorage } from './UserContext';
+
+function Rotas() {
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <UserStorage> */}
+          {/* <Header /> */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/login/*" element={<Login />} /> */}
+          </Routes>
+          {/* <Footer /> */}
+        {/* </UserStorage> */}
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default Rotas;
