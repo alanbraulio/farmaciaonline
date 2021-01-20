483c8e7b2a34c1249075c4decab35f32951300fe
Alan Braulio <alan-braulio@hotmail.com>
2021-01-20 00:53:34 -0300

    Colocando o oi

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,6 +12,7 @@ const Login = () => {
       <div className={styles.forms}>
         <Routes>
           {/* <Route path="/" element={<LoginForm />} /> */}
+          <p>Oi</p>
           <Route path="criar" element={<LoginCreate />} />
           <Route path="perdeu" element={<LoginPasswordLost />} />
           <Route path="resetar" element={<LoginPasswordReset />} />
