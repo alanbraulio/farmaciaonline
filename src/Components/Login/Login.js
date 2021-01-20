e046d1415122d1d9564124d9a73ac7df6e350904
Alan Braulio <alan-braulio@hotmail.com>
2021-01-20 00:31:09 -0300

    Removendo duplicata

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -12,7 +12,6 @@ const Login = () => {
       <div className={styles.forms}>
         <Routes>
           {/* <Route path="/" element={<LoginForm />} /> */}
-          {/* <Route path="/" element={<LoginForm />} /> */}
           <Route path="criar" element={<LoginCreate />} />
           <Route path="perdeu" element={<LoginPasswordLost />} />
           <Route path="resetar" element={<LoginPasswordReset />} />
