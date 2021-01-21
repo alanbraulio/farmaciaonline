50be58876c8d6738e0c91ca8e660452277f3af67
Alan Braulio <alan-braulio@hotmail.com>
2021-01-21 19:53:29 -0300

    Alterações para testar

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from '../Forms/Input/Input';
 
 const Home = () => {
-  return <div>
-    Home
-  </div>;
+
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [show, setShow] = useState(false);
+
+  return(
+
+  <div>
+      <form>
+        <Input type="text" name="username"/>
+        <button>{show ? 'Entrar' : 'Sair'}</button>
+      </form>
+    
+  </div>
+  );
 };
 
 export default Home;
